ea9d2174b91bfb532f8fbcd747b9e7f0db9238de
Aziz-Jalleli <azizjallali.99@gmail.com>
2025-04-07 11:02:27 +0100

    Initial commit

---
+++ src/main/java/org/example/projecthubbe/ProjectHubBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.projecthubbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHubBeApplication.class, args);
+	}
+
+}
